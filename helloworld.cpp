9bbe5e4274470008dc9fc80f6717c05fb5adc6e0
Tharkus <lucasguilherme_@hotmail.com>
2016-03-30 21:24:02 -0300

    Create helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,9 @@
+git clone para clonar
+
+git status para ver se tem alterações ficara vermelho
+
+git add nomedapastaouarquivo
+
+git commit -am 'comentarioquequisercolocar'
+
+git push origin master
